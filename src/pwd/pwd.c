feb177c4948c6c08abfe62080018b06d782cdf20
pdrago <jupurity6@gmail.com>
2024-02-02 11:31:40 -0300

    fix: malloc validation

--- src/pwd/pwd.c
+++ src/pwd/pwd.c
@@ -1,13 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 11:23:12 by pdrago            #+#    #+#             */
+/*   Updated: 2024/02/02 11:23:24 by pdrago           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
 int	main(void)
 {
 	char	*buffer;
-	int	size;
+	int		size;
 
 	size = 100;
 	buffer = (char *) malloc(sizeof(char) * size);
-	while(!getcwd(buffer, size))
+	if (!buffer)
+		return (1);
+	while (!getcwd(buffer, size))
 	{
 		free(buffer);
 		size += 100;
