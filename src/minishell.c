aebc4b1bc355e000b221c88c72a6a8ae932aa396
pdrago <jupurity6@gmail.com>
2024-02-02 11:20:45 -0300

    remove useless lines

--- src/minishell.c
+++ src/minishell.c
@@ -6,12 +6,11 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:29:05 by pdrago            #+#    #+#             */
-/*   Updated: 2024/02/01 16:29:42 by pdrago           ###   ########.fr       */
+/*   Updated: 2024/02/02 11:19:56 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-#include <sys/stat.h>
 
 int	main(void)
 {
@@ -19,12 +18,11 @@ int	main(void)
 	int		history_fd;
 
 	history_fd = open("history", O_RDWR | O_APPEND | O_CREAT, 0777);
+	printf("%i\n", history_fd);
 	while (TRUE)
 	{
 		prompt = readline("$ ");
 		if (register_command(prompt, history_fd) < 0)
 			return (1);
 	}
-	(void) history_fd;
-	(void) prompt;
 }
