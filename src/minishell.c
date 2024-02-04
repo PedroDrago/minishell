79feda99ab50c89604ddf0a0189200fa1d2af823
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-04 18:45:57 -0300

    feat: unclosed quotes and invalid characters validations

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,42 @@
 /*   By: pdrago <pdrago@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:29:05 by pdrago            #+#    #+#             */
-/*   Updated: 2024/02/02 11:21:24 by pdrago           ###   ########.fr       */
+/*   Updated: 2024/02/04 18:45:46 by pdrago           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	valid_characters(char *prompt)
+{
+	int	single_quotes;
+	int	double_quotes;
+	int	count;
+
+	single_quotes = 0;
+	double_quotes = 0;
+	count = 0;
+	while (prompt[count])
+	{
+		if (prompt[count] == '\\' || prompt[count] == ';')
+			return (FALSE);
+		else if (prompt[count] == '\'')
+			single_quotes++;
+		else if (prompt[count] == '"')
+			double_quotes++;
+		count++;
+	}
+	if (single_quotes % 2 != 0 || double_quotes % 2 != 0)
+		return (FALSE);
+	return (TRUE);
+}
+
+int	valid_prompt(char *prompt)
+{
+	if (!valid_characters(prompt))
+		return (FALSE);
+	return (TRUE);
+}
 
 int	main(void)
 {
@@ -23,5 +54,10 @@ int	main(void)
 		prompt = readline("$ ");
 		if (!register_command(prompt, history_fd)) 
 			return (1);
+		if (!valid_prompt(prompt))
+		{
+			write(2, "Minishell: Invalid Prompt\n", 26);
+			continue;
+		}
 	}
 }
