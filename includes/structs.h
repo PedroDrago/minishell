07e17c9345182ee92996210414d6b43f26b289ea
rodriguesrafaelm <rafaelmrodrigues96@hotmail.com>
2024-03-01 18:23:48 -0300

    feat: add env functions

--- includes/structs.h
+++ includes/structs.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafaelro <rafaelro@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 16:48:38 by rafaelro          #+#    #+#             */
+/*   Updated: 2024/03/01 16:48:39 by rafaelro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
@@ -22,4 +34,12 @@ typedef struct s_list
 	t_node *head;
 	t_node *tail;
 }	t_list;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
 #endif
