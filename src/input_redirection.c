7e47b3f71192d36c95f1bcb4b36c1ba44c302744
pdrago <jupurity6@gmail.com>
2024-03-25 13:33:35 -0300

    functional input redirect and heredoc

--- src/input_redirection.c
+++ src/input_redirection.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <fcntl.h>
+#include <readline/history.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int	is_redirect_input(char *token)
 {
@@ -24,4 +30,87 @@ int	is_heredoc(char *token)
 	if (!ft_strncmp(token, "<<", 3))
 		return (TRUE);
 	return (FALSE);
+}
+
+void	read_heredoc(int *file, char *delimiter)
+{
+	char *prompt;
+	int	len;
+
+	prompt = readline("> ");
+	len = ft_strlen(delimiter);
+	while (ft_strncmp(prompt, delimiter, len))
+	{
+		ft_putstr_fd(prompt, file[1]); // FIX: ctrl+D e ctrl+C?
+		ft_putstr_fd("\n", file[1]);
+		prompt = readline("> ");
+	}
+}
+
+int	heredoc(t_node *current, t_shell *shell, int fd_out)
+{
+	int	file[2];
+	int	pid;
+	int	status;
+
+
+	pipe(file);
+	read_heredoc(file, current->next->command);
+	if (is_builtin(current->command)) // WARN: No builtin reads from stdin, so redirecting input does nothing | Also, this will segfault if current.next is null
+		return (exec_builtin(current, shell, fd_out), close(file[0]), close(file[1]), TRUE);
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(file[0], 0);
+			dup2(fd_out, 1);
+			close(file[1]);
+			execute_command(shell, current);
+		}
+		else
+		{
+			status = 0;
+			close(file[1]);
+			close(file[0]);
+			waitpid(pid, &status, 0);
+			set_exit_status(status, shell);
+			if (status > 0)
+				resolve_error(status, current);
+		}
+	}
+	return (TRUE);
+}
+
+int	redirect_input(t_node *current, t_shell *shell, int fd_out)
+{
+	int	file;
+	int	pid;
+	int	status;
+
+	if (is_builtin(current->command)) // WARN: No builtin reads from stdin, so redirecting input does nothing | Also, this will segfault if current.next is null
+		return (exec_builtin(current, shell, fd_out), TRUE);
+	file = open(current->next->command, O_RDONLY);
+	if (file < 0)
+		return (ft_putstr_fd("No file or no permission\n", fd_out), FALSE); // FIX: error?
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(file, 0);
+			dup2(fd_out, 1);
+			execute_command(shell, current);
+		}
+		else
+		{
+			status = 0;
+			close(file);
+			waitpid(pid, &status, 0);
+			set_exit_status(status, shell);
+			if (status > 0)
+				resolve_error(status, current);
+		}
+	}
+	return (TRUE);
 }
