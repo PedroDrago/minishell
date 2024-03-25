2d3150f8d64a7eff98244fd33a7017672ad1fccb
pdrago <jupurity6@gmail.com>
2024-03-25 15:17:27 -0300

    fix double token problem

--- src/parser.c
+++ src/parser.c
@@ -62,15 +62,18 @@ int	fill_list(char **splited, t_list *list)
 		node = create_node();
 		if (!node)
 			return (FALSE);
-		node->command = ft_strdup(*splited);
-		splited = parse_arguments(splited, node);
-		if (!splited)
-			return (FALSE);
-		if (!(*splited))
-			node->token = NULL;
-		else
-			node->token = ft_strdup(*splited);
-		append_node(list, node);
+		if (!is_token(*splited))
+		{
+			node->command = ft_strdup(*splited);
+			splited = parse_arguments(splited, node);
+			if (!splited)
+				return (FALSE);
+			if (!(*splited))
+				node->token = NULL;
+			else
+				node->token = ft_strdup(*splited);
+			append_node(list, node);
+		}
 		if (!(*splited++))
 			return (TRUE);
 	}
