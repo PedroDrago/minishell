6299ffa56359593463451ae2638a6cdb855d2796
rodriguesrafaelm <rodriguesrafaelm@outlook.com>
2024-04-12 10:20:14 -0300

    refactor: perform_builtin_redirections() -10 lines

--- src/builtins.c
+++ src/builtins.c
@@ -6,48 +6,38 @@
 /*   By: rafaelro <rafaelro@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:17:33 by pdrago            #+#    #+#             */
-/*   Updated: 2024/04/12 09:54:33 by rafaelro         ###   ########.fr       */
+/*   Updated: 2024/04/12 10:16:49 by rafaelro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <sys/wait.h>
 
-int	perform_builtin_redirections(char **splited_command)
+void	perform_builtin_redirections(char **splited_com, int *status, int original_fd) // Como remover 4 linhas disso aqui?
 {
-	int	i;
-	int	status;
-	int	original_fd;
-	int	pid;
-
-	i = 0;
-	original_fd = dup(0);
-	status = 0;
-	while (splited_command[i])
+	while (*splited_com)
 	{
-		if (is_redirect_input(splited_command[i]))
+		if (is_redirect_input(*splited_com))
 		{
-			status = redirect_input_builtin(splited_command[++i]);
-			if (status)
-				return (status);
+			*status = redirect_input_builtin(*(++splited_com));
+			if (*status)
+				return ;
 		}
-		else if (is_redirect_output(splited_command[i]))
+		else if (is_redirect_output(*splited_com))
 		{
-			status = redirect_output_builtin(splited_command[i],
-					splited_command[i + 1]);
-			if (++i && status)
-				return (status);
+			*status = redirect_output_builtin(*splited_com, *(splited_com + 1));
+			if (++splited_com && status)
+				return ;
 		}
-		else if(is_heredoc(splited_command[i]))
+		else if(is_heredoc(*splited_com))
 		{
-			pid = fork();
-			if (pid == 0)
-				do_heredoc_builtin(splited_command[++i], original_fd);
-			waitpid(pid, &status, 0);
+			*status = fork();
+			if (*status == 0)
+				do_heredoc_builtin(*(++splited_com), original_fd);
+			waitpid(*status, status, 0);
 		}
-		i++;
+		++splited_com;
 	}
-	return (status);
 }
 
 int	is_builtin(char *command)
@@ -72,12 +62,15 @@ int	is_builtin(char *command)
 int	prep_builtin(t_node *node) //NOTE: Yeah bitch, char pointer pointer pointer
 {
 	int	status;
+	int original_fd;
 
+	status = 0;
+	original_fd = dup(0);
 	if (node->has_pipe)
 		dup2(node->node_pipe[1], 1);
 	if (node->prev && node->prev->has_pipe)
 		dup2(node->node_pipe[0], 0);
-	status = perform_builtin_redirections(node->splited_command);
+	perform_builtin_redirections(node->splited_command, &status, original_fd);
 	return (status);
 }
 
